8bebc8db5200fc3b3780d8b9ddc3826787b33891
martha <chavezmartha808@gmail.com>
2023-11-14 19:18:00 -0500

    examen02

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Instituto Superior Honorio Delgado Espinoza</h2>
+        <h4>Curasi Chavez Mertha Victoria</h4>
+        <h4>NOCHE</h4>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
